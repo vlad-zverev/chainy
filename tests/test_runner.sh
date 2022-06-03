ab8367777faaed760cd270ae2ab180e16372b4ca
vzverev <vzverev@freetechs.org>
2022-06-03 20:39:16 +0300

    mvp

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q) --force
-docker volume rm $(docker volume ls -q --filter dangling=true)
-docker network prune --force
-docker-compose up -d --no-recreate
+
+docker-compose down
+docker-compose pull
+docker-compose up -d --build
 docker-compose logs -f tests
